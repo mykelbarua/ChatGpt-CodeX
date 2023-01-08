b52c02ccdca9bb70c5a59d229f473a3dda6a9c7e
Mykel Baua <mykelbarua00@gmail.com>
2023-01-09 03:23:06 +0600

    New server url added

--- client/main.js
+++ client/main.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chatgpt-codex-blvs.onrender.com/", {
     method: "POST",
     headers: {
       "content-type": "application/json",
